31efb68848bbb90d14d358fa8a2b18499bf38059
Leonard Kinday <leonard@kinday.se>
2023-01-12 09:59:11 +0100

    Extract versioning to GitHub action

---
+++ actions/versions/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+PREV_VERSION="$(git describe --tags --abbrev=0 || echo "")"
+NEXT_VERSION="v$(date +"%Y%m%d.%H.%M%S")"
+
+echo "previous=${PREV_VERSION}" >> $GITHUB_OUTPUT
+
+PREV_SHA="$(git rev-parse "${PREV_VERSION}" || echo "")"
+if [[ "${PREV_SHA}" != "$(git rev-parse HEAD)" ]]; then
+  echo "next=${NEXT_VERSION}" >> $GITHUB_OUTPUT
+fi
